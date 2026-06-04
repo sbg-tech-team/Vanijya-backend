fa8c1d779d97e8639f8b0ff770e04755c5084726
Sanket Suryawanshi <suryawanshi6613@gmail.com>
2026-06-04 10:17:26 +0530

    Added stub migration for missing prod

---
+++ alembic/versions/c2d3e4f5a6b7_stub_restore_missing_revision.py
@@ -0,0 +1,20 @@
+"""stub: restore missing revision recorded in production DB
+
+Revision ID: c2d3e4f5a6b7
+Revises: b1c2d3e4f5a6
+Create Date: 2026-06-04
+"""
+from alembic import op
+
+revision = 'c2d3e4f5a6b7'
+down_revision = 'b1c2d3e4f5a6'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
