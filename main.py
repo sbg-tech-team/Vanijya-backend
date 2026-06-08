b5343aed86cb007caefc449d28910ec936681c70
Aadya Pande <aadyapande.in@gmail.com>
2026-06-08 11:21:52 +0530

    Commented unstaged developments

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ from app.modules.connections.router import (
 )
 from app.modules.news.router import router as news_router
 from app.modules.feed.router import router as feed_router
-from app.modules.chat.presentation.router import router as chat_router
-from app.modules.chat.presentation.ws_router import ws_router as chat_ws_router
+# from app.modules.chat.presentation.router import router as chat_router
+# from app.modules.chat.presentation.ws_router import ws_router as chat_ws_router
 from app.modules.deeplink.router import router as deeplink_router
 from app.modules.safety.router import router as safety_router
 from app.modules.verification.router import router as verification_router
@@ -55,8 +55,8 @@ app.include_router(connections_router)
 app.include_router(recommendations_router)
 app.include_router(news_router)
 app.include_router(feed_router)
-app.include_router(chat_router)
-app.include_router(chat_ws_router)
+# app.include_router(chat_router)
+# app.include_router(chat_ws_router)
 app.include_router(deeplink_router)
 app.include_router(safety_router)
 app.include_router(verification_router)
